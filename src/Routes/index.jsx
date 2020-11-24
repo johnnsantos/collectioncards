44b4f08e0c1a8c6d805b72611793198f37dc302b
Johnnatan <contatojohnnatan@gmail.com>
2020-11-24 11:47:19 -0300

    feat: add menu topbar

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -5,10 +5,17 @@ import FavoritePokemon from "../pages/FavoritePokemon";
 import FavoriteRickAndMorty from "../pages/FavoriteRickAndMorty";
 import PokemonsPage from "../pages/PokemonsPage";
 import RickAndMortyPage from "../pages/RickAndMortyPage";
+import SearchBar from "../components/SearchBar";
+import Menu from "../components/Menu";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const Routes = () => {
   return (
-    <AnimatePresence>
+    // <AnimatePresence>
+    <>
+      <CssBaseline />
+      <Menu />
+      <SearchBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/favoritepokemon" component={FavoritePokemon} />
@@ -16,7 +23,8 @@ const Routes = () => {
         <Route path="/pokemonspage" component={PokemonsPage} />
         <Route path="/rickandmortypage" component={RickAndMortyPage} />
       </Switch>
-    </AnimatePresence>
+    </>
+    // </AnimatePresence>
   );
 };
 
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input></input>
+    </div>
+  );
 };
 
 export default SearchBar;
